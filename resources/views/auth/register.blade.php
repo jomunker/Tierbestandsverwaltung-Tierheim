a2402994fa8c8038da1aae2c2ab94a5c915df1b9
Johannes <johannes.munker@gmail.com>
2020-08-05 20:35:48 +0200

    some other optimizations

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
-@section('content')
+{{-- @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -74,4 +75,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
